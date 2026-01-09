37d0f9e04ff070f94611e82ce0d1c062aeb4f693
SergiV <svinent21176@iesjoanramis.org>
2026-01-09 17:19:43 +0100

    UD3ex2: Desplegar submenus i canviar fletcha up & down

--- UD3/ex2/index.js
+++ UD3/ex2/index.js
@@ -1,8 +1,16 @@
 const btnMenu = document.querySelector("#btn-menu");
 const btnClose = document.querySelector("#btn-close");
 const navMenu = document.querySelector("#nav-menu");
+
 const btnFeatures = document.querySelector("#btn-features");
 const ulFeatures = document.querySelector(".ul-features");
+const upFeatures = document.querySelector("#arrow-up-1");
+const downFeatures = document.querySelector("#arrow-down-1");
+
+const btnCompany = document.querySelector("#btn-company");
+const ulCompany = document.querySelector(".ul-company");
+const upCompany = document.querySelector("#arrow-up-2");
+const downCompany = document.querySelector("#arrow-down-2");
 
 // Abrir menu hamburguesa
 btnMenu.addEventListener("click", ()=>{
@@ -17,4 +25,13 @@ btnClose.addEventListener("click", ()=>{
 // Abrir o cerrar apartado "Features" en menu hamburguesa
 btnFeatures.addEventListener("click", ()=>{
     ulFeatures.classList.toggle("display-flex");
+    downFeatures.classList.toggle("display-none");
+    upFeatures.classList.toggle("display-inline");
+});
+
+// Abrir o cerrar apartado "Company" en menu hamburguesa
+btnCompany.addEventListener("click", ()=>{
+    ulCompany.classList.toggle("display-flex");
+    downCompany.classList.toggle("display-none");
+    upCompany.classList.toggle("display-inline");
 });

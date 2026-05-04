8f8f87eacc8a5329647598a1eb8a70191ac7e1ff
SergiV <svinent21176@iesjoanramis.org>
2026-05-04 19:37:06 +0200

    UD5ex2_4: Solucionar

--- UD5/UD5ex2/UD5ex2_4.js
+++ UD5/UD5ex2/UD5ex2_4.js
@@ -15,6 +15,7 @@ let textNode = document.createTextNode("Hola!");
 holaFinal.appendChild(textNode);
 container.appendChild(holaFinal);
 
+container.innerHTML += "adios"; //Permet afegir texte, pero no és per posar objectes més complicats
 
 // f) Afegeix la classe “main” al div amb una classe “footer”.
 const footer = document.querySelector("div.footer");
@@ -27,18 +28,29 @@ const li = document.createElement("li");
 textNode = document.createTextNode("four");
 li.appendChild(textNode);
 
-const ol = document.querySelector("ol");
-ol.appendChild(li);
+const ul = document.querySelector("ul");
+ul.appendChild(li);
 
 // i) Passa per tots els elements de la llista ordenada i dona’ls un color de fons de color "verd".
 const lis = document.querySelectorAll("ol li");
-lis.forEach(element => {
-    element.style.backgroundColor = "green";
-});
+lis.forEach(element => element.style.backgroundColor = "green");
 
 // j) Elimina el div amb una classe “footer”.
 footer.remove();
 // k) Inserta un nou element abans del primer li de la llista no ordenada.
 const primerLiUl = document.createElement("li");
-const ul = document.querySelector("ul");
+primerLiUl.appendChild(document.createTextNode("aa"))
 ul.insertBefore(primerLiUl, ul.firstElementChild);
+
+
+// l) Mou l'ultim element de la llista no ordenada a la llist aordenada
+const ultim = document.querySelector("ul li:last-child");
+
+const ol = document.querySelector("ol");
+ol.appendChild(ultim);
+
+// m) Clona el primer element de la llist ano ordenada i afegeix-lo al final
+const ul_first = document.querySelector("ul li:first-child");
+const clone = ul_first.cloneNode(true); //El true hace que copie también el texto de dentro y no solo el Nodo
+
+ul.appendChild(clone); 
